9aa09f8775e318009170da1f749ed36794688f85
Brennan Beam <BrennanBeam@gmail.com>
2024-05-07 10:28:35 -0700

    Additional dotnet demo methods.

--- ConsumeDotNetAOT/src/main/java/org/example/DotNetWrap.java
+++ ConsumeDotNetAOT/src/main/java/org/example/DotNetWrap.java
@@ -7,4 +7,7 @@ public interface DotNetWrap extends Library {
     String pathToLib = "C:\\Programs\\Git\\Repos\\NativeLibrary\\NativeLibrary\\bin\\Release\\net8.0\\win-x64\\native\\NativeLibrary.dll";
     DotNetWrap INSTANCE = Native.load(pathToLib,DotNetWrap.class);
     int aotsample_add(int a, int b);
+    int aotsample_write_line(String s);
+    String aotsample_sumstring(String one, String two);
 }
+
--- ConsumeDotNetAOT/src/main/java/org/example/Main.java
+++ ConsumeDotNetAOT/src/main/java/org/example/Main.java
@@ -2,8 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Java works fine");
+        System.out.println("Java");
         int result = DotNetWrap.INSTANCE.aotsample_add(1,1); // 1 + 1 = 2 please god.
-        System.out.println("Who cares, we didn't break! But the answer was " + result);
+        System.out.println( "DotNet returned an int: " + result);
+        int successIs0 = DotNetWrap.INSTANCE.aotsample_write_line("Hello from DotNet!");
+        String concatString = DotNetWrap.INSTANCE.aotsample_sumstring("DotNet ", "Returned a String!");
+        System.out.println(concatString);
     }
 }
